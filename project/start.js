3cf42f18394e9c1257d991ef24b2083837a015a2
elvisvrosa <81196221+elvisvrosa@users.noreply.github.com>
2023-03-31 14:04:16 -0400

    Updated port

--- project/start.js
+++ project/start.js
@@ -1,22 +1,21 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.set('port', (5500 || process.env.PORT));
+app.set("port", process.env.PORT || 5500);
 
 app.use(express.static(__dirname));
 
 // views is directory for all template files
-app.set('views', __dirname + '/html');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/html");
+app.set("view engine", "ejs");
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get("/", function (request, response) {
+  response.render("pages/index");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function () {
+  console.log("Node app is running on port", app.get("port"));
 });
-
 
 // This file is what handles incoming requests and
 // serves files to the browser, or executes server-side code
